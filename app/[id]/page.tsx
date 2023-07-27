b8f9defa3e19c98b96e1a90e1f8ef07b362f94a7
alihaghjou <alihaghjou30@gmail.com>
2023-07-27 16:15:15 +0330

    env added

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,19 +1,15 @@
 import DetailPage from "@/Components/DetailPage";
 import { datatype } from "../api/country/route";
 
-
-
 async function getData(id: string) {
-    const data = await fetch(`${process.env.URL_API}/api/country`);
-    const res: datatype = await data.json();
-    const item = res.filter(item => item.alpha3Code === id)[0]
-    return item;
-  }
+  const data = await fetch(`${process.env.URL}/api/country`);
+  const res: datatype = await data.json();
+  const item = res.filter((item) => item.alpha3Code === id)[0];
+  return item;
+}
 
-export default async function Home({params}: {params: {id: string}}) {
-    const country = await getData(params.id)
-    
-  return (
-    <DetailPage country={country} />
-  )
+export default async function Home({ params }: { params: { id: string } }) {
+  const country = await getData(params.id);
+
+  return <DetailPage country={country} />;
 }
